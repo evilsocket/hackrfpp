7d5018f2597b4fbaf298ede0cc1b385ab3d91ba3
evilsocket <evilsocket@gmail.com>
2015-04-17 16:36:54 +0300

    dunno dunno dunno

--- hackrfpp.hpp
+++ hackrfpp.hpp
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2015, Simone Margaritelli <evilsocket at gmail dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of HackRF++ nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 #ifndef HACKRF_H
 #define HACKRF_H
 
@@ -14,12 +42,12 @@
 typedef std::complex<float> complex_t;
 
 template<class DEMOD>
-class HackRF
+class HackRFPP
 {
 public:
 
-    HackRF();
-    virtual ~HackRF();
+    HackRFPP();
+    virtual ~HackRFPP();
 
     void open();
 
@@ -52,7 +80,7 @@ protected:
     }
 
 template<class DEMOD>
-HackRF<DEMOD>::HackRF() : _device(NULL), _running(false) {
+HackRFPP<DEMOD>::HackRFPP() : _device(NULL), _running(false) {
     // create a lookup table for complex values
     _lookup_table.reserve( 0xffff + 1 );
     for( uint32_t i = 0; i <= 0xffff; ++i ) {
@@ -64,7 +92,7 @@ HackRF<DEMOD>::HackRF() : _device(NULL), _running(false) {
 }
 
 template<class DEMOD>
-void HackRF<DEMOD>::open() {
+void HackRFPP<DEMOD>::open() {
     int status = -1;
 
     status = hackrf_init();
@@ -75,55 +103,55 @@ void HackRF<DEMOD>::open() {
 }
 
 template<class DEMOD>
-void HackRF<DEMOD>::set_frequency( uint64_t freq ) {
+void HackRFPP<DEMOD>::set_frequency( uint64_t freq ) {
     int status = hackrf_set_freq( _device, freq );
     HACKRF_CHECK_STATUS( status, "Failed to set frequency." );
 }
 
 template<class DEMOD>
-void HackRF<DEMOD>::set_sample_rate( uint64_t srate ) {
+void HackRFPP<DEMOD>::set_sample_rate( uint64_t srate ) {
     int status = hackrf_set_sample_rate( _device, srate );
     HACKRF_CHECK_STATUS( status, "Failed to set sample rate." );
 }
 
 template<class DEMOD>
-void HackRF<DEMOD>::set_amp_enabled( bool enabled ) {
+void HackRFPP<DEMOD>::set_amp_enabled( bool enabled ) {
     int status = hackrf_set_amp_enable( _device, enabled ? 1 : 0 );
     HACKRF_CHECK_STATUS( status, "Failed to set AMP status." );
 }
 
 template<class DEMOD>
-void HackRF<DEMOD>::set_lna_gain( uint32_t gain ) {
+void HackRFPP<DEMOD>::set_lna_gain( uint32_t gain ) {
     int status = hackrf_set_lna_gain( _device, gain );
     HACKRF_CHECK_STATUS( status, "Failed to set LNA gain." );
 }
 
 template<class DEMOD>
-void HackRF<DEMOD>::set_vga_gain( uint32_t gain ) {
+void HackRFPP<DEMOD>::set_vga_gain( uint32_t gain ) {
     int status = hackrf_set_vga_gain( _device, gain );
     HACKRF_CHECK_STATUS( status, "Failed to set VGA gain." );
 }
 
 template<class DEMOD>
-void HackRF<DEMOD>::start() {
+void HackRFPP<DEMOD>::start() {
     int status = hackrf_start_rx( _device, rx_callback, this );
     HACKRF_CHECK_STATUS( status, "Failed to start RX streaming." );
     _running = true;
 }
 
 template<class DEMOD>
-void HackRF<DEMOD>::stop() {
+void HackRFPP<DEMOD>::stop() {
     _running = false;
     hackrf_stop_rx(_device);
 }
 
 template<class DEMOD>
-bool HackRF<DEMOD>::is_streaming() const {
+bool HackRFPP<DEMOD>::is_streaming() const {
     return ( hackrf_is_streaming(_device) == HACKRF_TRUE );
 }
 
 template<class DEMOD>
-HackRF<DEMOD>::~HackRF() {
+HackRFPP<DEMOD>::~HackRFPP() {
     if( _device ) {
         stop();
         hackrf_close(_device);
@@ -133,8 +161,8 @@ HackRF<DEMOD>::~HackRF() {
 }
 
 template<class DEMOD>
-int HackRF<DEMOD>::rx_callback(hackrf_transfer *transfer) {
-    HackRF *hrf = (HackRF *)transfer->rx_ctx;
+int HackRFPP<DEMOD>::rx_callback(hackrf_transfer *transfer) {
+    HackRFPP *hrf = (HackRFPP *)transfer->rx_ctx;
 
     if( hrf->_running ) {
         unsigned short *p = (unsigned short *)transfer->buffer;
--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
+/*
+ * Copyright (c) 2015, Simone Margaritelli <evilsocket at gmail dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of HackRF++ nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 #include <stdio.h>
 #include <signal.h>
 #include <iostream>
-#include "hackrf.hpp"
+#include "hackrfpp.hpp"
 
 struct AM {
     void demodulate( const std::vector<complex_t>& data ) {
@@ -22,12 +50,12 @@ struct AM {
     }
 };
 
-HackRF<AM> hrf;
+HackRFPP<AM> dev;
 
 void signal_handler(int dummy) {
     printf( "\n@ Received CTRL+C\n" );
 
-    hrf.stop();
+    dev.stop();
 }
 
 int main( int argc, char **argv )
@@ -36,17 +64,17 @@ int main( int argc, char **argv )
 
     try
     {
-        hrf.open();
+        dev.open();
 
-        hrf.set_frequency( 13.56 * 1e6 );
-        hrf.set_sample_rate( 8 * 1e6 );
-        hrf.set_amp_enabled( false );
-        hrf.set_lna_gain( 32 );
-        hrf.set_vga_gain( 30 );
+        dev.set_frequency( 13.56 * 1e6 );
+        dev.set_sample_rate( 8 * 1e6 );
+        dev.set_amp_enabled( false );
+        dev.set_lna_gain( 32 );
+        dev.set_vga_gain( 30 );
 
-        hrf.start();
+        dev.start();
 
-        while( hrf.is_streaming() ) {
+        while( dev.is_streaming() ) {
             usleep(100);
         }
     }
